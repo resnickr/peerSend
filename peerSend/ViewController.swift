ff28caf63564a13c5db8ce5448adfa62c88ebc33
Ryan Resnick <ryanresnick@phantom.local>
2018-11-14 11:44:55 -0800

    initial commit

---
+++ peerSend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  peerSend
+//
+//  Created by Ryan Resnick on 11/14/18.
+//  Copyright © 2018 ghost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
